26085163f80ca8231c31da1850599bb6bde3cb5b
micaela-torres <torresmicaela2111@gmail.com>
2022-07-03 17:53:22 -0300

    ingresando el comportamiento del alumno

--- js/archivo.js
+++ js/archivo.js
@@ -1,8 +1,26 @@
 ///SABER MI PROMEDIO
-do {
-    nota1 = parseFloat(prompt("Ingrese la nota numero 1")); 
-    nota2 = parseFloat(prompt("Ingrese la nota numero 2"));
-    nota3 = parseFloat(prompt("Ingrese la nota numero 3"));
-}while(isNaN(nota3) || isNaN(nota2) || isNaN(nota3));
 
-alert ((nota1 + nota2 + nota3) / 3);
+const conductamuybuena = 10;
+const conductabuena = 7;
+const conductamala = 4;
+
+do {
+    nota1 = parseFloat(prompt("Ingrese la nota examen 1")); 
+    nota2 = parseFloat(prompt("Ingrese la nota examen 2"));
+    nota3 = parseFloat(prompt("Ingrese la nota examen 3"));
+    nota4 = prompt("Ingrese conducta. Muy Buena, Buena o Mala").toLowerCase();
+     if (nota4 === "muy buena"){
+        nota4 = conductamuybuena;
+     }else if (nota4 === "buena"){
+        nota4 = conductabuena;
+     }else if (nota4 === "mala"){
+        nota4 = conductamala;
+     }else{
+        alert ("Ingrese una conducta valida")
+     }
+
+     
+
+}while(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4));
+
+alert ((nota1 + nota2 + nota3 + nota4) / 4);
